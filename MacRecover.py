98c1170cbe6eca8149a290339417861724eb7ba9
RyanisyydsTT <ryantseng123@go.edu.tw>
2024-08-17 11:37:47 +0800

    First Commit

---
+++ MacRecover.py
@@ -0,0 +1,62 @@
+import os
+import subprocess
+print("""
+ __  __            ____                                ____       
+|  \/  | __ _  ___|  _ \ ___  ___ _____   _____ _ __  |  _ \ _   _
+| |\/| |/ _` |/ __| |_) / _ \/ __/ _ \ \ / / _ \ '__| | |_) | | | |
+| |  | | (_| | (__|  _ <  __/ (_| (_) \ V /  __/ |    |  __/| |_| |
+|_|  |_|\__,_|\___|_| \_\___|\___\___/ \_/ \___|_|    |_|    \__, |
+                                                              |___/ 
+""")
+print("This is a tool to help you recover your Mac.")
+print("This project was made by github.com/RyanisyydsTT")
+print("""
+[MacOS installer downloader]
+1. Big Sur (11.7.0)
+2. Monterey (12.7.6)
+3. Ventura (13.6.9)
+4. Sonoma (14.6.1)
+[We will upload more version to here after first release!]
+Please select the version you want to download
+""")
+selection = input(">>> ")
+if selection == 1:
+    os = "MacOS Big Sur"
+    print(f"Let me confirm, You are downloading {os}, and the file will be stored in the same directory as the python file, if confirmed, Please input Y/N")
+    confirmed = input(">>> ")
+    if confirmed == "Y" or "y":
+        subprocess.run(['curl', ['-O'], 'https://swcdn.apple.com/content/downloads/14/38/042-45246-A_NLFOFLCJFZ/jk992zbv98sdzz3rgc7mrccjl3l22ruk1c/InstallAssistant.pkg'])
+    else:
+        print("Not confirmed, quitting")
+        os.quit(1)
+elif selection == 2:
+    os = "MacOS Monterey"
+    print(f"Let me confirm, You are downloading {os}, and the file will be stored in the same directory as the python file, if confirmed, Please input Y/N")
+    confirmed = input(">>> ")
+    if confirmed == "Y" or "y":
+        subprocess.run(['curl', ['-O'], 'https://swcdn.apple.com/content/downloads/34/21/062-40406-A_GZQ27OUQER/ggclib72ow1omcvfexvp84bc9x5ei5tyqu/InstallAssistant.pkg'])
+    else:
+        print("Not confirmed, quitting")
+        os.quit(1)
+elif selection == 3:
+    os = "MacOS Ventura"
+    print(f"Let me confirm, You are downloading {os}, and the file will be stored in the same directory as the python file, if confirmed, Please input Y/N")
+    confirmed = input(">>> ")
+    if confirmed == "Y" or "y":
+        subprocess.run(['curl', ['-O'], 'https://swcdn.apple.com/content/downloads/34/21/062-40406-A_GZQ27OUQER/ggclib72ow1omcvfexvp84bc9x5ei5tyqu/InstallAssistant.pkg'])
+    else:
+        print("Not confirmed, quitting")
+        os.quit(1)
+elif selection == 4:
+    os = "MacOS Sonoma"
+    print(f"Let me confirm, You are downloading {os}, and the file will be stored in the same directory as the python file, if confirmed, Please input Y/N")
+    confirmed = input(">>> ")
+    if confirmed == "Y" or "y":
+        subprocess.run(['curl', ['-O'], 'https://swcdn.apple.com/content/downloads/34/21/062-40406-A_GZQ27OUQER/ggclib72ow1omcvfexvp84bc9x5ei5tyqu/InstallAssistant.pkg'])
+    else:
+        print("Not confirmed, quitting")
+        os.quit(1)
+else:
+    print("Wrong input, please try again")
+    os.quit(1)
+
---
+++ README.md
@@ -0,0 +1,50 @@
+# MacRecoverPy
+
+MacRecoverPy is a tool designed to help users recover their Mac by downloading macOS installer packages.
+
+## Features
+
+- Downloads macOS installer packages for various versions:
+  - Big Sur (11.7.0)
+  - Monterey (12.7.6)
+  - Ventura (13.6.9)
+  - Sonoma (14.6.1)
+  - More will be added in next release!
+- User Friendly Menu
+- No Virus, installer sourse is from apple
+
+## Usage
+
+1. Run the script: `python MacRecover.py`
+2. Select the desired macOS version by entering the corresponding number
+3. Confirm your selection
+4. Wait for the download to complete
+
+## Requirements
+
+- Python 3.x
+- `curl` command-line tool (usually pre-installed on macOS)
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Report bugs
+
+If you encounter any issues or bugs while using MacRecoverPy, please report them on our GitHub Issues page:
+
+[Report a bug](https://github.com/RyanisyydsTT/MacRecoverPy/issues)
+
+When reporting a bug, please include:
+- A clear description of the problem
+- Steps to reproduce the issue
+- Your system information (macOS version, Python version)
+- Any error messages or screenshots, if applicable
+
+Your feedback helps improve MacRecoverPy for everyone. Thank you for your contribution!
+
+Created by [RyanisyydsTT](https://github.com/RyanisyydsTT)
+
+## Disclaimer
+
+This tool is provided as-is, without any warranties. Always ensure you have a backup of your important data before attempting any system recovery or reinstallation.
